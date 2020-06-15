3a58204a9fa83cd4a3f1d34e92cf015733556aeb
Giux <giuliamelody@gmail.com>
2020-06-15 17:21:23 +0200

    toFixed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var sconto;
+
 //chiedere all'utente dove vuole andare (in km)
 var distanzaKm = parseInt (prompt ("Quanti km vorresti percorrere oggi? "));
 
@@ -5,14 +7,23 @@ var distanzaKm = parseInt (prompt ("Quanti km vorresti percorrere oggi? "));
 var etaUtente = prompt ("Quanti anni hai? ");
 
 //calcolare il prezzo totale del viaggio
-
 var biglietto = 0.21 * distanzaKm;
 
-console.log(biglietto);
-document.getElementById('titolo').innerHTML = "Il prezzo del biglietto è di " + biglietto + "€";
 
 if (etaUtente < 18) {
-  biglietto - 20%
-} else (etaUtente > 65) {
-  biglietto - 40% ;
+  sconto=(biglietto*0.2) ;
+} else if (etaUtente>65) {
+  sconto=(biglietto*0.4) ;
+} else {
+  sconto=0;
 }
+
+// var b = biglietto.toFixed(1);
+// var s = sconto.toFixed(2);
+ Math.floor(biglietto);
+ Math.floor(sconto);
+
+
+console.log(biglietto);
+console.log(sconto);
+document.getElementById('titolo').innerHTML = "Il prezzo del biglietto è di " + ( biglietto - sconto ) + "€";
